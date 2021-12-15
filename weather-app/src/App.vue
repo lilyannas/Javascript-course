92b52ff1da3906bef3883a7b81e3ad666011bce4
unknown <micposso@gmail.com>
2021-12-15 00:02:32 -0500

    add css styles

---
+++ weather-app/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <SearchField></SearchField>
+    <Tempeture></Tempeture>
+  </div>
+</template>
+
+<script>
+import SearchField from './components/SearchField.vue'
+import Tempeture from './components/Tempeture.vue'
+
+let weatherAPI = 'fe412c491c7b80ec2c62af05b01e2511';
+
+export default {
+  name: 'App',
+  components: {
+    SearchField,
+    Tempeture
+  },
+  data() {
+    return {
+      api_key: weatherAPI
+    }
+  }
+
+}
+
+</script>
+<style>
+  @import './assets/styles.css';
+</style>
+
+
---
+++ weather-app/src/components/SearchField.vue
@@ -0,0 +1,17 @@
+<template>
+<main>
+  <div class="search-box">
+    <input type="text" class="search-bar" placeholder="Search for city..." />
+  </div>
+</main>
+</template>
+
+
+<script>
+export default {
+  name: 'SearchField',
+  props: {
+    msg: String
+  }
+}
+</script>
