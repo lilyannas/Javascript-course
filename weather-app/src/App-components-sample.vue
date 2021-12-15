12d1d3341d147f9d854ec9b7d3040806256e3c82
unknown <micposso@gmail.com>
2021-12-15 07:45:37 -0500

    add sample components files

---
+++ weather-app/src/App-components-sample.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''">
+    <main>
+      <div class="search-box">
+        <input 
+          type="text" 
+          class="search-bar" 
+          placeholder="Search..."
+          v-model="query"
+          @keypress="fetchWeather"
+        />
+      </div>
+
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+        <div class="location-box">
+          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+          <div class="date">{{ dateBuilder() }}</div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp">{{ Math.round(weather.main.temp) }}°</div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+
+let weatherAPI = "YOUR_API_KEY_HERE";
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      api_key: weatherAPI,
+      url_base: 'https://api.openweathermap.org/data/2.5/',
+      query: '',
+      weather: {}
+    }
+  },
+  methods: {
+    fetchWeather (e) {
+      if (e.key == "Enter") {
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+          .then(res => {
+            return res.json();
+          }).then(this.setResults);
+      }
+    },
+    setResults (results) {
+      this.weather = results;
+    },
+    dateBuilder () {
+      let d = new Date();
+      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return `${day} ${date} ${month} ${year}`;
+    }
+  }
+}
+</script>
+<style>
+  @import './assets/styles.css';
+</style>
---
+++ weather-app/src/components/DisplayWeather.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+    <div class="location-box">
+      <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+      <div class="date">{{ dateBuilder() }}</div>
+    </div>
+
+    <div class="weather-box">
+      <div class="temp">{{ Math.round(weather.main.temp) }}°</div>
+      <div class="weather">{{ weather.weather[0].main }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "displaWeather",
+  props: {
+      weather: {
+          Type: () => ({}), // type object
+          default: {}
+      }
+  },
+  methods: {
+    dateBuilder() {
+      let d = new Date();
+      let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+      let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return `${day} ${date} ${month} ${year}`;
+    },
+  },
+};
+</script>
